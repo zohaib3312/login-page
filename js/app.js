4712af60950b77d7f9af3ea8c531d96449b36ed8
zohaib akhtar <za809475@gmail.com>
2023-03-31 19:58:58 +0500

    login page complete

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function myfunction(){
+            
+    var uname = document.getElementById("uname").value;
+    var upass = document.getElementById("upass").value;
+
+    if ( uname == "zohaib akhtar"  &&  upass == 12345   ) {
+        
+        alert(" you sign in susscessfully.......! ")
+
+    }else{
+
+        alert("Invalid username and password");
+    }
+
+}
